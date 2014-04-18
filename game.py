6df05f6553305ce5569298b919a762eab7cd2b1d
Mark Vasilkov <mvasilkov@gmail.com>
2014-04-19 00:38:16 +0300

    multiquad.glsl

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from kivy.clock import Clock
 from kivy.graphics.instructions import RenderContext
 from kivy.uix.widget import Widget
 
@@ -6,5 +7,10 @@ class Game(Widget):
     '''Game renderer'''
 
     def __init__(self, **kwargs):
-        self.canvas = RenderContext()
+        self.canvas = RenderContext(use_parent_projection=True)
+        self.canvas.shader.source = 'multiquad.glsl'
         Widget.__init__(self, **kwargs)
+        Clock.schedule_interval(self.update_glsl, 60 ** -1)
+
+    def update_glsl(self, nap):
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import kivy
 kivy.require('1.8.0')
 
+from kivy import utils
 from kivy.app import App
 from kivy.base import EventLoop
 from kivy.config import Config
@@ -17,5 +18,11 @@ class KivyApp(App):
         return Game()
 
 if __name__ == '__main__':
+    Config.set('graphics', 'width', '960')
+    Config.set('graphics', 'height', '540')  # 16:9
     Config.set('input', 'mouse', 'mouse,disable_multitouch')
+
+    from kivy.core.window import Window
+    Window.clearcolor = utils.get_color_from_hex('#242424')
+
     KivyApp().run()
