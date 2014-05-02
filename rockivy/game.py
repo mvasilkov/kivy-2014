07afc11bc4e93ddb39b1e72902787b370d0d601a
Mark Vasilkov <mvasilkov@gmail.com>
2014-05-02 23:46:14 +0300

    Fretboard

--- rockivy/game.py
+++ rockivy/game.py
@@ -1,6 +1,4 @@
 from collections import namedtuple
-import math
-from random import random as rand, choice as randc
 
 from kivy.clock import Clock
 from kivy.graphics import Mesh
@@ -12,9 +10,51 @@ from .util import load_tex_uv
 CURSOR_OFFSET_X = 16
 CURSOR_OFFSET_Y = -16
 
+FRET_COUNT = 16
+FRET_SPACING = 55
+
+STRING_COUNT = 6
+STRING_SPACING = 30
+
+FRET_LENGTH = (STRING_COUNT - 1) * STRING_SPACING
+STRING_LENGTH = FRET_COUNT * FRET_SPACING
+
+FB_OFFSET_LEFT = (960 - STRING_LENGTH) * 0.5
+FB_OFFSET_BOTTOM = (540 - FRET_LENGTH) * 0.5
+
 R9 = namedtuple('Renderable', 'x y rot size op tex')
 
 g_window = None
+
+
+def mutate(t, changes):
+    res = list(t)
+    for idx, val in changes.iteritems():
+        res[idx] = val
+    return tuple(res)
+
+
+def update_tex_uv(tex_uv):
+    tex_uv['fret'] = mutate(tex_uv['fret'], {5: FRET_LENGTH * 0.5})
+
+    tex_uv.update([('string_%d' % c,
+                    mutate(tex_uv['string'],
+                           {4: STRING_LENGTH * 0.5 + tex_uv['fret'][4],
+                            5: (c >> 1) + 1})) for c in xrange(STRING_COUNT)])
+
+
+def init_frets():
+    return [R9(x=c * FRET_SPACING + FB_OFFSET_LEFT,
+               y=FRET_LENGTH * 0.5 + FB_OFFSET_BOTTOM,
+               rot=0, size=1, op=1, tex='fret')
+            for c in xrange(FRET_COUNT + 1)]
+
+
+def init_strings():
+    return [R9(x=STRING_LENGTH * 0.5 + FB_OFFSET_LEFT,
+               y=FRET_LENGTH - (c * STRING_SPACING) + FB_OFFSET_BOTTOM,
+               rot=0, size=1, op=1, tex='string_%d' % c)
+            for c in xrange(STRING_COUNT)]
 
 
 class Game(Widget):
@@ -35,6 +75,10 @@ class Game(Widget):
             ('vTexCoords0', 2, 'float'),
         )
         self.tex, self.tex_uv = load_tex_uv('a.atlas')
+        update_tex_uv(self.tex_uv)
+
+        self.frets = init_frets()
+        self.strings = init_strings()
 
         Clock.schedule_interval(self.update_glsl, 60 ** -1)
 
@@ -44,22 +88,12 @@ class Game(Widget):
 
     def update_glsl(self, nap):
         self.canvas.clear()
-        objects = self.random_fill(96)
+        objects = self.frets + self.strings
         cur_x, cur_y = g_window.mouse_pos
         objects.append(R9(x=cur_x + CURSOR_OFFSET_X,
                           y=cur_y + CURSOR_OFFSET_Y,
                           rot=0, size=1, op=1, tex='cursor'))
         self.render(objects)
-
-    def random_fill(self, count):
-        width, height = self.size
-        tex_list = self.tex_uv.keys()
-        return [R9(x=rand() * width,
-                   y=rand() * height,
-                   rot=rand() * math.pi * 2,
-                   size=rand() + 0.5,
-                   op=rand() + 0.5,
-                   tex=randc(tex_list)) for c in xrange(count)]
 
     def render(self, objects):
         indices = []
